f102e395f70534c8dc0dc2a6f760df1340fd7dd5
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-09-09 16:42:30 +0530

    update

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
 import './Nav.css';
+import { useHistory } from 'react-router-dom';
 
 function Nav() {
+    const history = useHistory();
 
     return (
         <div className="nav ">
@@ -12,10 +14,10 @@ function Nav() {
             </div>
             <div className="links">
                 <ul className="myclass">
-                    <li><Link exact activeClassName="active" to='/home'>Home</Link></li>
+                    <li><Link exact activeClassName="active" to='/'>Home</Link></li>
                     <li><Link activeClassName="active" to='/'>Get UId</Link></li>
                     <li><Link activeClassName="active" to='/'>About</Link></li>
-                    <li><Link activeClassName="active" to='/'>Login</Link></li>
+                    <li><Link activeClassName="active" onClick={() => history.push("/Login")}>Login</Link></li>
                 </ul>
             </div>
         </div>
